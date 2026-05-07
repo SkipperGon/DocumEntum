63de8dc0fd04d3bc476412efebdb6ccbc602d32a
FoxDip <FoxDip@DESKTOP-P6BJJQR>
2026-05-07 12:51:48 +0300

    отображение ролей

---
+++ DocumEntum/Services/IOrganizationService.cs
@@ -0,0 +1,15 @@
+﻿using DocumEntum.Data;
+
+namespace DocumEntum.Services
+{
+    public interface IOrganizationService
+    {
+        Task<List<Department>> GetAllDepartmentsAsync();
+        Task<Department> CreateDepartmentAsync(string name, int? parentId);
+        Task DeleteDepartmentAsync(int id);
+        Task<List<Employee>> GetAllEmployeesAsync();
+        Task AssignEmployeeToPositionAsync(int employeeId, int departmentId, int positionId);
+
+
+    }
+}
--- DocumEntum/Services/OrganizationService.cs
+++ DocumEntum/Services/OrganizationService.cs
@@ -3,14 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DocumEntum.Services
 {
-    public interface IOrganizationService
-    {
-        Task<List<Department>> GetAllDepartmentsAsync();
-        Task<Department> CreateDepartmentAsync(string name, int? parentId);
-        Task DeleteDepartmentAsync(int id);
-        Task<List<Employee>> GetAllEmployeesAsync();
-        Task AssignEmployeeToPositionAsync(int employeeId, int departmentId, int positionId);
-    }
 
     public class OrganizationService : IOrganizationService
     {
